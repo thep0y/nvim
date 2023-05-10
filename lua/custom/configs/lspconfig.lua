c4ef8eef3f4651578a8cbea878cdf2034f2be0ff
thepoy <thepoy@163.com>
2023-05-10 23:44:00 +0800

    添加bash、lua的lsp配置
    Signed-off-by: thepoy <thepoy@163.com>

--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,10 +3,16 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = { "pyright", "gopls", "tsserver", "volar", "eslint", "stylelint_lsp" }
+local servers = { "pyright", "gopls", "tsserver", "volar", "eslint", "stylelint_lsp", "bashls", "lua_ls" }
 
 
 for _, lsp in ipairs(servers) do
   local opts = require("custom.configs." .. lsp)
   lspconfig[lsp].setup(opts)
 end
+
+
+
+
+
+
