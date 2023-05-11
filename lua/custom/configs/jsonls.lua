659107b5765a9d9d8c021ba61971776811ca5b30
thepoy <thepoy@163.com>
2023-05-11 09:45:53 +0800

    支持 json-lsp

---
+++ lua/custom/configs/jsonls.lua
@@ -0,0 +1,24 @@
+local capabilities = require("plugins.configs.lspconfig").capabilities
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local options = {
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end
+      })
+    end
+  end
+}
+
+return options
--- lua/custom/configs/lspconfig.lua
+++ lua/custom/configs/lspconfig.lua
@@ -3,16 +3,10 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 
-local servers = { "pyright", "gopls", "tsserver", "volar", "eslint", "stylelint_lsp", "bashls", "lua_ls" }
+local servers = { "pyright", "gopls", "tsserver", "volar", "eslint", "stylelint_lsp", "bashls", "lua_ls", "jsonls" }
 
 
 for _, lsp in ipairs(servers) do
   local opts = require("custom.configs." .. lsp)
   lspconfig[lsp].setup(opts)
 end
-
-
-
-
-
-
