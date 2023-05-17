bfc7ff4c2d5ad66dc45551f1a44dfb22ef5dc23e
thepoy <thepoy@163.com>
2023-05-17 15:57:35 +0800

    添加浮窗相关快捷键

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,10 +8,14 @@ M.crates = {
       end,
       "update crates"
     },
+    ["<leader>d"] = { "\"_d", "delete lines without copying" },
+    ["<leader>o"] = { ":lua vim.diagnostic.open_float()<CR>", "Show all diagnostics on current line in floating window" },
+    ["<leader>on"] = { ":lua vim.diagnostic.goto_next()<CR>", "Show all diagnostics on current line in floating window" },
+    ["<leader>op"] = { ":lua vim.diagnostic.goto_prev()<CR>", "Show all diagnostics on current line in floating window" },
     ["<C-s>"] = { ":w<CR>", "save" },
     ["<A-j>"] = { ":m .+1<CR>==", "move lines down" },
     ["<A-k>"] = { ":m .-2<CR>==", "move lines up" },
-    ["<leader>d"] = { "\"_d", "delete lines without copying" },
+    ["x"] = { "\"_x", "delete char without copying" },
   },
   i = {
     ["<C-s>"] = { "<Esc>:w<CR>a", "save" },
