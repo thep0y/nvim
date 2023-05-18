5d1b4f1cb54e72e0338ae4a4ba972ee7a60d87d2
thepoy <thepoy@163.com>
2023-05-18 17:36:39 +0800

    '修改主题'

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,6 +1,10 @@
 ---@type ChadrcConfig 
  local M = {}
- M.ui = {theme = 'onedark'}
+ M.ui = {
+  theme_toggle = { "ayu_dark", "ayu_light"},
+  theme = "ayu_light",
+}
+
  M.plugins = 'custom.plugins'
  M.mappings = require("custom.mappings")
  return M
