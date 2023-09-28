e6db001771eb8f39930bd91b3f5a05f4f3404674
Ryan Wang <thepoy@163.com>
2023-09-28 20:40:29 +0800

    默认安装一些语法

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,5 +1,20 @@
 local options = {
-  ensure_installed = { "lua" },
+  ensure_installed = {
+    "lua",
+    "python",
+    "rust",
+    "javascript",
+    "typescript",
+    "tsx",
+    "sql",
+    "yaml",
+    "toml",
+    "json",
+    "markdown",
+    "vue",
+    "html",
+    "go",
+  },
 
   highlight = {
     enable = true,
