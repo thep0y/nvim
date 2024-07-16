494e65da6fd9e83efbc37cf01ec83f6cd64cb928
Ryan Wang <thepoy@163.com>
2024-07-16 10:18:24 +0800

    修改 neovide macos option 配置

--- init.lua
+++ init.lua
@@ -40,5 +40,5 @@ end)
 
 if vim.g.neovide then
   vim.o.guifont = "Monaco Nerd Font:h16"
-  vim.g.neovide_input_macos_alt_is_meta = true
+  vim.g.neovide_input_macos_option_key_is_meta = "only_left"
 end
